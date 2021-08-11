e1e55e0d64ff2edff008ca8baff2d2d53e06d1c6
Niraj Bhavnani <niraj.bhavnani@morningstar.com>
2021-08-11 22:46:09 +0530

    UC3: Email Regex

--- index.js
+++ index.js
@@ -19,3 +19,16 @@ if(lastNameRegex.test(lastNameStr)){
 else{
     console.log('Invalid');
 }
+
+//As a User need to enter a valid email
+// - E.g. abc.xyz@bl.co.in 
+// - Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions
+var email = "niraj.bhavnani@ves.ac.in";
+var emailRegex = /^[A-Za-z_0-9]+[.]?[A-Za-z_0-9]+[@][A-Za-z0-9]+[.](\w){2,3}[.]?(\w?){2,3}$/;
+
+if(emailRegex.test(email)){
+    console.log('Valid');
+}
+else{
+    console.log('Invalid');
+}
